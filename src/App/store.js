85bdbc95b615ca839f5227bd7ad58fc871a631b8
adhed <adamroweb@gmail.com>
2017-04-01 23:18:14 +0200

    add localStorage support

--- src/App/store.js
+++ src/App/store.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
-const store = createStore(rootReducer);
+const persistedState = loadState();
+const store = createStore(
+    rootReducer,
+    persistedState
+ );
+ 
+ store.subscribe(throttle(() => {
+     saveState({
+         tasks: store.getState().tasks
+     })
+ }, 1000));
 
 export default store;
